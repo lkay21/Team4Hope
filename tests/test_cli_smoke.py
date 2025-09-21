d492e2a068652589d6725843229dcbfa3a22f6d5
Noah Joseph Grzegorek <ngrzegor@eceprog4.ecn.purdue.edu>
2025-09-21 18:00:35 -0400

    pipe fix

--- tests/test_cli_smoke.py
+++ tests/test_cli_smoke.py
@@ -1,8 +1,8 @@
 from subprocess import run, PIPE
-import ast
+import json
 def test_run_smoke():
     r = run(["./run", "https://huggingface.co/google-bert/bert-base-uncased"], stdout=PIPE, text=True)
     assert r.returncode == 0
-    rec =  ast.literal_eval(r.stdout.strip())
+    rec = json.loads(r.stdout.strip())
     assert rec["name"] == "bert-base-uncased"
     assert rec["category"] == "MODEL"
