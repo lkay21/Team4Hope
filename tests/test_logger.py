45cd99a1f1a4b48b7875d56db06df7ac2160eee5
Logan Charles Kay <kay21@eceprog4.ecn.purdue.edu>
2025-09-23 20:49:08 -0400

    fixed naming in test file

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -5,8 +5,8 @@ from src.logger import get_logger
 def test_logger_writes_to_file(tmp_path):
     # Point LOG_PATH to a temp file pytest gives us
     log_file = tmp_path / "out.log"
-    os.environ["LOG_FILE"] = str(log_file)
-    os.environ["LOG_LEVEL"] = "2"  # debug level
+    os.environ["LOG_PATH"] = str(log_file)
+    os.environ["LOG_VERBOSITY"] = "2"  # debug level
 
     logger = get_logger("test")
     logger.debug("hello world")
@@ -18,8 +18,8 @@ def test_logger_writes_to_file(tmp_path):
 
 def test_logger_respects_verbosity(tmp_path, caplog):
     # Verbosity 0 = WARNING and above only
-    os.environ["LOG_LEVEL"] = "0"
-    os.environ.pop("LOG_FILE", None)  # no file, console only
+    os.environ["LOG_VERBOSITY"] = "0"
+    os.environ.pop("LOG_PATH", None)  # no file, console only
 
     logger = get_logger("test_console")
     logger.debug("should not show up")
@@ -31,7 +31,7 @@ def test_logger_respects_verbosity(tmp_path, caplog):
     assert "this should show up" in messages
 
     # Now test info level
-    os.environ["LOG_LEVEL"] = "1"
+    os.environ["LOG_VERBOSITY"] = "1"
     logger = get_logger("test_console_info")
     logger.info("info message")
     logger.warning("warning message")
