a68b67b0ae4a690da0780414883a368ba5017fa5
Syed Yahya Quadri <quadris@eceprog1.ecn.purdue.edu>
2025-09-25 20:19:49 -0400

    Had to add one test to make it work

---
+++ tests/test_temp.py
@@ -0,0 +1,7 @@
+def test_temp():
+    """A trivial smoke test to ensure pytest collects at least one test.
+
+    This prevents CI from failing when the rest of the test suite has been
+    removed temporarily.
+    """
+    assert True
