2e4b74fd53cbca0dfc4036a962819539d6520a6f
Syed Yahya Quadri <quadris@eceprog1.ecn.purdue.edu>
2025-09-25 20:05:17 -0400

    First series of deletion

--- tests/test_cli_smoke.py
+++
@@ -1,8 +0,0 @@
-# from subprocess import run, PIPE
-# import json
-# def test_run_smoke():
-#     r = run(["./run", "https://huggingface.co/google-bert/bert-base-uncased"], stdout=PIPE, text=True)
-#     assert r.returncode == 0
-#     rec = json.loads(r.stdout.strip())
-#     assert rec["name"] == "bert-base-uncased"
-#     assert rec["category"] == "MODEL"
