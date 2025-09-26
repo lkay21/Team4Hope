cc2879031ba320ff826a3d355204cd5201bc7e18
Syed Yahya Quadri <quadris@eceprog1.ecn.purdue.edu>
2025-09-25 20:25:11 -0400

    Deleting Scripts Folder

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "🧹 Cleaning old coverage data..."
-rm -f .coverage .coverage.* 
-rm -rf htmlcov/
-
-echo "✅ Running tests with coverage..."
-COVERAGE_FILE=/tmp/.coverage pytest --cov=src --cov-report=term-missing "$@"
